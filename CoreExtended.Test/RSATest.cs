5f2355edfef51710b910e4d75801e5435aa9dd4a
fanpian <532726345@qq.com>
2020-11-27 18:06:11 +0800

    2.0.1 C# RSA原始的方式加解密，支持分段加密、分段解密

--- CoreExtended.Test/RSATest.cs
+++ CoreExtended.Test/RSATest.cs
@@ -3,6 +3,8 @@ using System.IO;
 using CoreExtended.Encrypt.RSAEncrypt;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using CoreExtended.Extend;
+using System.Security.Cryptography;
+using CoreExtended.Encrypt;
 
 namespace CoreExtended.Test
 {
@@ -24,10 +26,30 @@ namespace CoreExtended.Test
         public void publicKey()
         {
             string publickey = RSAExtend.ToPEM_PKCS8();
-            RSA rsa = new RSA(publickey, true);
-            string str = rsa.Encode("这是一个测试字符串!");
+            CoreExtended.Encrypt.RSAEncrypt.RSA rsa = new CoreExtended.Encrypt.RSAEncrypt.RSA(publickey, true);
+            string str = rsa.Encode("乐凯医疗科技有限公司-云影像平台");
 
             string decodeStr = RSAExtend.Decode(str);
+        }
+
+        [TestMethod]
+        public void CreatePrivateKey()
+        {
+            string privateFile = Path.Combine(@"C:\Users\fp532\Desktop", "privateKey.xml");
+            string publicFile = Path.Combine(@"C:\Users\fp532\Desktop", "publicKey.xml");
+            RSACryptoServiceProvider ras = new RSACryptoServiceProvider();
+            string privateKey = ras.ToXmlString(true);
+            File.WriteAllText(privateFile, privateKey);
+            string publicKey = ras.ToXmlString(false);
+            File.WriteAllText(publicFile, publicKey);
+        }
+
+        [TestMethod]
+        public void Decrypt()
+        {
+            string encryptStr = RSAXmlOption.Encrypt("乐凯医疗科技有限公司 - 云影像平台");
+
+            string str = RSAXmlOption.Decrypt(encryptStr);
         }
     }
 }
